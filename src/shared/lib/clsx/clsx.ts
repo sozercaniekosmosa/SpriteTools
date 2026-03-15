1d43fcb001c9f86c9a7008f19b8c7ce1f4a1d32b
cherezov.vn <cherezov.vn@gazprom-neft.ru>
2026-03-15 16:45:27 +0500

    feat: public prepare

--- src/shared/lib/clsx/clsx.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-/**
- * Merges class names and handles Tailwind CSS conflicts.
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
--- src/shared/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './sprite-extractor/sprite-extractor';
-export * from './cocos-extractor/cocos-extractor';
-export * from './button/Button';
